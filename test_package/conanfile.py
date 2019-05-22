6e46289b422e646913db979d37bcc6bbb68b805b
SSE4 <tomskside@gmail.com>
2019-05-22 17:21:11 +0200

    - verify doxygen actuall works
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 
 class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build(target="doxygen")
 
     def test(self):
         self.output.info("Version:")
