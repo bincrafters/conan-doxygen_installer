d6b32ec1b7743652a40d83308a38080842d6aa00
SSE4 <tomskside@gmail.com>
2019-06-10 10:28:23 +0200

    - disable testing CMake integration, it's broken with old CMake on appveyor
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake
+from conans import ConanFile
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build(target="doxygen")
-
     def test(self):
         self.output.info("Version:")
         self.run("doxygen --version")
